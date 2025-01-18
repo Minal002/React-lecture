2144ba2b32eceec9ff38c3ca5042362c670117c0
Minal002 <minal2576@gmail.com>
2025-01-18 23:17:33 +0900

    page

---
+++ next/next-fresh/app/list/page.tsx
@@ -0,0 +1,24 @@
+interface Product {
+  id: number;
+  name: string;
+  price: number;
+}
+
+export default function Home() {
+  const foodstuffs: Product[] = [
+    { id: 1, name: 'Tomato', price: 40 },
+    { id: 2, name: 'Potato', price: 40 },
+    { id: 3, name: 'Carrot', price: 40 }
+  ];
+  
+  return (
+    <div>
+      <h4 className="title">상품목록</h4>
+      {foodstuffs.map((foodstuff: Product) => (
+        <div className="food" key={foodstuff.id}>
+          <h4>{foodstuff.name} ${foodstuff.price}</h4>
+        </div>
+      ))}
+    </div>
+  );
+}
---
+++ next/next-fresh/app/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  let name: string = 'kim'
+  return (
+    <div>
+      <h3 className="title">fresh</h3>
+      <p className="title-sub">by dev { name }</p> 
+    </div>
+  );
+}
